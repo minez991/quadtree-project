9a2a4163d65b5322d41dc7308e9edc0fe33008de
minez991 <peterfu00@gmail.com>
2018-11-19 13:34:36 +0000

    new

--- program/program.c
+++ program/program.c
@@ -2,38 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-struct Point{
-  int x,y,color;
-};
-typedef struct quadnode {
-  struct Point* topLeft;
-  struct Point* bottomRight;
-  int max_x,max_y,min_x,min_y,color;
-  struct quadnode* parentnode;
-  struct quadnode* NW;
-  struct quadnode* NE;
-  struct quadnode* SE;
-  struct quadnode* SW;
-}quadnode;
 
-int logof2(int width){
-  int n = 0;
-  do{
-    if (width != 1){
-      n = n+1;
-      width = width / 2;
-    }
-  }while(width != 1);
-  return n;
-}
-
-void readarray(int width, int image_array[][width],int black){
+void readarray(int width, int image_array[][width],FILE* fp){
   int x,y,scan;
   do{
-    scan = scanf("%i %i",&x,&y);
+    scan = fscanf(fp,"%i %i",&x,&y);
     image_array[x][y] = 1;
-    black = black -1;
-  }while (black != 0);
+  }while (scan != EOF);
 
   for (x = 0;x < width; x++){
     for (y = 0; y < width;y++){
@@ -58,55 +33,25 @@ void printarray(int width, int image_array[][width]){
     printf("\n-----------------\n");
   }
 }
-/*
-int calculatecolor(int width, int image_array[][width], quadnode* node){
-  int colorsum  = 0;
-  int nodeN  = 0;
-  int nodecolor;
-  int x,y;
-  printf("\n\n\nI am here\n\n\n");
-  for (x = node->min_y ; x <= node->max_x; x++){
-    for (y = node ->min_y;y <= node->max_y ; y++){
-      colorsum = colorsum + image_array[x][y];
-      nodeN = nodeN + 1;
-    }
-  }
-  nodecolor = colorsum/nodeN;
-  return nodecolor;
-
-}
-int buildtree(int width,int image_array[][width],quadnode* node,int level){
-  int maxlevel = logof2(width);
-  if (level == 0){
-    return 1;
-  }
-  return 0;
-}
-
-void quadtree(int width,int image_array[][width],quadnode* node){
-  printf("\n\n\nI am here\n\n\n");
-
-  printf("\n\n\nI am here\n\n\n");
 
 
-  calculatecolor(width,image_array,node);
-  buildtree(width,image_array,node,0);
-
-}
-*/
-int main(){
+int main(int argc, char *argv[]){
   /*setting variables for working*/
   int width,B_num;
-  scanf("%i",&width);
-  scanf("%i",&B_num);
-  //quadnode* node;
-  //node->max_x = 0;
+  FILE* fp;
+  fp = fopen("./input/input.txt","r");
+  if (fp== NULL ){
+    fprintf(stderr,"unable to read the file\n");
+    exit(-1);
+  }
+  fscanf(fp,"%i",&width);
+  fscanf(fp,"%i",&B_num);
+  //quadnode img;
   int image_array[width][width];
 /*read and print data*/
-  readarray(width, image_array,B_num);
+  readarray(width, image_array,fp);
   printarray(width,image_array);
-
 /*compress data*/
-  //quadtree(width,image_array,node);
+
   return 0;
 }
